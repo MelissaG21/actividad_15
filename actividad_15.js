c2a9ba3c77873295129fb8a02e8c1954fa82360c
Melissa G <a334259@uach.mx>
2021-05-05 05:48:45 -0600

    Se agrego la actividad 15

---
+++ actividad_15.js
@@ -0,0 +1,46 @@
+const url = 'https://jsonplaceholder.typicode.com/photos';
+const lista = document.getElementById('lista');
+
+/*fetch(url)
+    .then(response => response.json())
+    .then(fotos => {
+        fotos.map(foto => {
+            let li = document.createElement("li");
+            li.innerHTML =  `<a href="detalle.html?id=${foto.id}">${foto.id}</a>${foto.title}`;
+            lista.appendChild(li);
+        })
+    })
+    .catch(error => {
+        document.getElementById('error').innerHTML = error.message;
+        console.log(error);
+    })
+    .finally(()=>{
+        document.getElementById('cargando').innerHTML="";
+        document.getElementById('cargandoGIF').src = "";
+        console.log("Proceso terminado")
+    });
+    */
+
+const obtenerFotos = async () => {
+    const response = await fetch(url);
+    const fotos = await response.json();
+    return fotos;
+}
+
+obtenerFotos()
+    .then(fotos => {
+        fotos.map(foto => {
+            let li = document.createElement("li");
+            li.innerHTML =  `<a href="detalle.html?id=${foto.id}">${foto.id}</a>${foto.title}`;
+            lista.appendChild(li);
+        })
+    })
+    .catch(error => {
+        document.getElementById('error').innerHTML = error.message;
+        console.log(error);
+    })
+    .finally(()=>{
+        document.getElementById('cargando').innerHTML="";
+        document.getElementById('cargandoGIF').src = "";
+        console.log("Proceso terminado")
+    });
